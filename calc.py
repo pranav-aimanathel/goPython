c5428f0d03f0b481819e4cd5898777327e4726b4
Pranav A L <pranaval2010@gmail.com>
2023-10-13 23:43:22 +0530

    Added division and subtraction to the operations

--- calc.py
+++ calc.py
@@ -4,13 +4,17 @@ def main():
     y = int(input("Second Number: "))
 
     #choose the operation
-    op = input("Add or Multi: ").capitalize()
+    op = input("Choose the operation: ").capitalize()
 
     #apply the selected operation
     if op == 'A':
         add(x, y)
     elif op == 'M':
         multi(x, y)
+    elif op == 'S':
+        sub(x, y)
+    elif op == 'D':
+        div(x, y)
     else:
         print("Invalid")
 
@@ -21,6 +25,14 @@ def multi(x, y):
 #define operation for addition
 def add(x, y):
     print(x + y)
+
+#define operation for subtraction
+def sub(x, y):
+    print(x - y)
+
+#define operation for division
+def div(x, y):
+    print(x / y)
     
 #call main
 main()
