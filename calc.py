90b1058922af4ea9526699d13548a06412f77449
Pranav A L <pranaval2010@gmail.com>
2023-10-13 23:20:21 +0530

    Add comments

--- calc.py
+++ calc.py
@@ -1,8 +1,12 @@
 def main():
+    #get the inputs
     x = int(input("First Number: "))
     y = int(input("Second Number: "))
+
+    #choose the operation
     op = input("Add or Multi: ").capitalize()
 
+    #apply the selected operation
     if op == 'A':
         add(x, y)
     elif op == 'M':
@@ -10,12 +14,13 @@ def main():
     else:
         print("Invalid")
 
-
+#define operation for multiplication
 def multi(x, y):
     print(x * y)
 
+#define operation for addition
 def add(x, y):
     print(x + y)
     
-
+#call main
 main()
